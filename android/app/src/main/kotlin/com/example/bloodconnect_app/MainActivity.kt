5b676e8e1695fb22fef94acd8f875eac3be109ed
Mohd Salauddin <sallumalik1111@gmail.com>
2026-02-19 17:19:33 +0530

    feat: integrate Firebase and user authentication

---
+++ android/app/src/main/kotlin/com/example/bloodconnect_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bloodconnect_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const LoginScreen(),
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final emailController = TextEditingController();
+  final passController = TextEditingController();
+
+  bool loading = false;
+
+  Future<void> login() async {
+    setState(() => loading = true);
+
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passController.text.trim(),
+      );
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const Dashboard()),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("Login Failed")));
+    }
+
+    setState(() => loading = false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("BloodConnect Login")),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(labelText: "Email"),
+            ),
+            TextField(
+              controller: passController,
+              obscureText: true,
+              decoration: const InputDecoration(labelText: "Password"),
+            ),
+            const SizedBox(height: 20),
+            loading
+                ? const CircularProgressIndicator()
+                : ElevatedButton(onPressed: login, child: const Text("Login")),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Dashboard")),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text("Add Blood Request"),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => const AddRequestScreen()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class AddRequestScreen extends StatefulWidget {
+  const AddRequestScreen({super.key});
+
+  @override
+  State<AddRequestScreen> createState() => _AddRequestScreenState();
+}
+
+class _AddRequestScreenState extends State<AddRequestScreen> {
+  final nameController = TextEditingController();
+  final bloodController = TextEditingController();
+  final cityController = TextEditingController();
+  final phoneController = TextEditingController();
+
+  bool loading = false;
+
+  Future<void> saveRequest() async {
+    setState(() => loading = true);
+
+    await FirebaseFirestore.instance.collection("requests").add({
+      "name": nameController.text,
+      "blood": bloodController.text,
+      "city": cityController.text,
+      "phone": phoneController.text,
+      "time": Timestamp.now(),
+    });
+
+    setState(() => loading = false);
+
+    Navigator.pop(context);
+
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(const SnackBar(content: Text("Request Saved")));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Add Blood Request")),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: const InputDecoration(labelText: "Patient Name"),
+            ),
+            TextField(
+              controller: bloodController,
+              decoration: const InputDecoration(labelText: "Blood Group"),
+            ),
+            TextField(
+              controller: cityController,
+              decoration: const InputDecoration(labelText: "City"),
+            ),
+            TextField(
+              controller: phoneController,
+              decoration: const InputDecoration(labelText: "Phone"),
+            ),
+            const SizedBox(height: 20),
+            loading
+                ? const CircularProgressIndicator()
+                : ElevatedButton(
+                    onPressed: saveRequest,
+                    child: const Text("Save Request"),
+                  ),
+          ],
+        ),
+      ),
+    );
+  }
+}
